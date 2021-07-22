9d08d682f857c8505147f089e21039cf32853c66
Afshin Arani <afshin@arani.dev>
2021-07-22 16:24:58 +0430

    Port some unused cells to F#

---
+++ NOnion/Cells/CellPadding.fs
@@ -0,0 +1,23 @@
+﻿namespace NOnion.Cells
+
+open System.IO
+
+open NOnion
+
+type CellPadding =
+    {
+        Bytes: array<byte>
+    }
+
+    static member Deserialize (reader: BinaryReader) =
+        {
+            Bytes = reader.ReadBytes Constants.FixedPayloadLength
+        }
+        :> ICell
+
+    interface ICell with
+
+        member __.Command = 0uy
+
+        member self.Serialize writer =
+            writer.Write self.Bytes
