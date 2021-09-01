af27912e96690d557450b540e521e2ccdcb2c8a5
Afshin Arani <afshin@arani.dev>
2021-09-01 14:24:48 +0430

    Tests: add retry to multi hop test

--- NOnion.Tests/MultihopCircuits.cs
+++ NOnion.Tests/MultihopCircuits.cs
@@ -11,21 +11,17 @@ namespace NOnion.Tests
 {
     public class MultihopCircuits
     {
+        /* It's possible that the router returned by GetRandomFallbackDirectory or
+         * GetRandomRoutersForDirectoryBrowsing be inaccessable so we need to continue
+         * retrying if an exceptions happened to make sure the issues are not related
+         * to the router we randomly chose
+         */
+        private const int TestRetryCount = 5;
 
-        [Test]
-        public async Task CanCreateMultiHopCircuits()
+        private async Task CreateMultiHopCircuits()
         {
             TestContext.Progress.WriteLine("Receiving descriptors...");
-            List<CircuitNodeDetail> nodes = null;
-            try
-            {
-                nodes = await CircuitHelper.GetRandomRoutersForDirectoryBrowsingWithRetry(3);
-            }
-            catch
-            {
-                Assert.Inconclusive();
-            }
-
+            List<CircuitNodeDetail> nodes = await CircuitHelper.GetRandomRoutersForDirectoryBrowsingWithRetry(3);
 
             TestContext.Progress.WriteLine($"Connecting to {nodes[0].Address.Value.Address}...");
             using TorGuard guard = await TorGuard.NewClientAsync(nodes[0].Address.Value);
@@ -47,6 +43,13 @@ namespace NOnion.Tests
             var response = await httpClient.GetAsStringAsync("/tor/status-vote/current/consensus", false);
 
             Assert.That(response.Contains("network-status-version"));
+        }
+
+        [Test]
+        [Retry(TestRetryCount)]
+        public void CanCreateMultiHopCircuits()
+        {
+            Assert.DoesNotThrowAsync(CreateMultiHopCircuits);
         }
 
     }
