1b6358e68bff07d51591219fbf136d0c5b5d7c8e
Afshin Arani <afshin@arani.dev>
2021-09-07 15:21:15 +0430

    Crypto: add support for SHA3-based mac

---
+++ NOnion/Crypto/HiddenServicesCipher.fs
@@ -0,0 +1,22 @@
+﻿namespace NOnion.Crypto
+
+open Org.BouncyCastle.Crypto.Digests
+
+open NOnion.Utility
+
+module HiddenServicesCipher =
+    let CalculateMacWithSHA3256 (key: array<byte>) (msg: array<byte>) =
+        let data =
+            let keyLenBytes =
+                key.LongLength
+                |> uint64
+                |> IntegerSerialization.FromUInt64ToBigEndianByteArray
+
+            Array.concat [ keyLenBytes; key; msg ]
+
+        let digestEngine = Sha3Digest ()
+        let output = Array.zeroCreate (digestEngine.GetDigestSize ())
+        digestEngine.BlockUpdate (data, 0, data.Length)
+        digestEngine.DoFinal (output, 0) |> ignore<int>
+
+        output
