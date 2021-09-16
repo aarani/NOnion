133b0be6f55958ef5010c2b329a79e0da0ca8e5b
Afshin Arani <afshin@arani.dev>
2021-09-16 19:05:29 +0430

    Crypto\HS: calc key blinding factor

--- NOnion/Crypto/HiddenServicesCipher.fs
+++ NOnion/Crypto/HiddenServicesCipher.fs
@@ -1,9 +1,12 @@
 ﻿namespace NOnion.Crypto
 
+open System.Text
+
 open Org.BouncyCastle.Crypto.Digests
 open Org.BouncyCastle.Crypto.Parameters
 open Org.BouncyCastle.Crypto.Signers
 
+open NOnion
 open NOnion.Utility
 
 module HiddenServicesCipher =
@@ -31,3 +34,45 @@ module HiddenServicesCipher =
         signer.Init (true, privateKey)
         signer.BlockUpdate (data, 0, data.Length)
         signer.GenerateSignature ()
+
+    let CalculateBlindingFactor
+        (periodNumber: uint64)
+        (periodLength: uint64)
+        (publicKey: array<byte>)
+        =
+        let nonce =
+            Array.concat
+                [
+                    "key-blind" |> Encoding.ASCII.GetBytes
+                    periodNumber
+                    |> IntegerSerialization.FromUInt64ToBigEndianByteArray
+                    periodLength
+                    |> IntegerSerialization.FromUInt64ToBigEndianByteArray
+                ]
+
+        let digestEngine = Sha3Digest ()
+        let output = Array.zeroCreate (digestEngine.GetDigestSize ())
+
+        digestEngine.BlockUpdate (
+            Constants.HiddenServiceBlindString,
+            0,
+            Constants.HiddenServiceBlindString.Length
+        )
+
+        digestEngine.BlockUpdate (publicKey, 0, publicKey.Length)
+
+        digestEngine.BlockUpdate (
+            Constants.Ed25519BasePointString,
+            0,
+            Constants.Ed25519BasePointString.Length
+        )
+
+        digestEngine.BlockUpdate (nonce, 0, nonce.Length)
+        digestEngine.DoFinal (output, 0) |> ignore<int>
+
+        //CLAMPING
+        output.[0] <- output.[0] &&& 248uy
+        output.[31] <- output.[31] &&& 63uy
+        output.[31] <- output.[31] ||| 64uy
+
+        output
