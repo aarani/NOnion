c06f98cbcadb4a6dce5dab9869898ec8b8ec842d
Afshin Arani <afshin@arani.dev>
2021-10-07 11:59:18 +0330

    TorCircuit: add callback for introduction requests

--- NOnion.Tests/HiddenServicesTests.cs
+++ NOnion.Tests/HiddenServicesTests.cs
@@ -11,6 +11,7 @@ using Org.BouncyCastle.Crypto;
 
 using NOnion.Network;
 using NOnion.Http;
+using NOnion.Cells.Relay;
 
 namespace NOnion.Tests
 {
@@ -30,13 +31,18 @@ namespace NOnion.Tests
             TorCircuit circuit = new(guard);
 
             await circuit.CreateAsync(CircuitNodeDetail.FastCreate);
-            await circuit.RegisterAsIntroductionPointAsync(FSharpOption<AsymmetricCipherKeyPair>.None);
+            await circuit.RegisterAsIntroductionPointAsync(FSharpOption<AsymmetricCipherKeyPair>.None, FuncConvert.ToFSharpFunc<RelayIntroduce, Task>(StubCallback));
+        }
+
+        private Task StubCallback(RelayIntroduce _)
+        {
+            return Task.CompletedTask;
         }
 
 
         [Test]
         [Retry(TestsRetryCount)]
-        public void CanCreateIntroductionCircuit ()
+        public void CanCreateIntroductionCircuit()
         {
             Assert.DoesNotThrowAsync(CreateIntroductionCircuit);
         }
