5cbb9dd1ff8f94a3cb757d016060fd3e0dcf3850
Afshin Arani <afshin@arani.dev>
2021-10-08 15:53:44 +0330

    wip2

--- NOnion/Services/TorServiceHost.fs
+++ NOnion/Services/TorServiceHost.fs
@@ -10,6 +10,11 @@ open Org.BouncyCastle.Security
 
 open NOnion.Cells.Relay
 open NOnion.Utility
+open Org.BouncyCastle.Crypto.Agreement
+open System.Text
+open Org.BouncyCastle.Crypto.Digests
+open NOnion.Directory
+open NOnion.Crypto
 
 type IntroductionPointInfo =
     {
@@ -17,6 +22,8 @@ type IntroductionPointInfo =
         EncryptionKey: AsymmetricCipherKeyPair
         AuthKey: AsymmetricCipherKeyPair
         NodeDetail: CircuitNodeDetail
+        Circuit: TorCircuit
+        Guard: TorGuard
     }
 
 type IntroductionPointPublicInfo =
@@ -28,12 +35,11 @@ type IntroductionPointPublicInfo =
     }
 
 
-type TorServiceHost () =
+type TorServiceHost (directory: TorDirectory, masterKey: Ed25519PublicKeyParameters) =
 
     let mutable introductionPointKeys: Map<string, IntroductionPointInfo> =
         Map.empty
 
-    let mutable guardNode: Option<TorGuard> = None
     let introductionPointSemaphore: SemaphoreLocker = SemaphoreLocker ()
 
     member private self.RelayIntroduceCallback (introduce: RelayIntroduce) =
@@ -48,9 +54,160 @@ type TorServiceHost () =
                 | (true, details) -> details
             | _ -> failwith "Unknown introduction point"
 
-        //TODO: Decrypt?
-        ()
+        let randomServerKey =
+            let kpGenX = X25519KeyPairGenerator ()
 
+            let random = SecureRandom ()
+
+            kpGenX.Init (X25519KeyGenerationParameters random)
+
+            kpGenX.GenerateKeyPair ()
+
+
+        async {
+            let keyAgreement = X25519Agreement ()
+            keyAgreement.Init introductionPointDetails.EncryptionKey.Private
+            let exp = Array.zeroCreate keyAgreement.AgreementSize
+            keyAgreement.CalculateAgreement (X25519PublicKeyParameters(introduce.ClientPublicKey, 0), exp, 0)
+            let ProtoId = "tor-hs-ntor-curve25519-sha3-256-1"
+
+            let m_hsexpand =
+                ProtoId + ":hs_key_expand" |> Encoding.ASCII.GetBytes
+
+            let t_hsenc =
+                ProtoId + ":hs_key_extract" |> Encoding.ASCII.GetBytes
+
+            let digestEngine = Sha3Digest ()
+
+            let credential =
+                digestEngine.GetByteLength () |> Array.zeroCreate
+
+            let credentialDigestInput =
+                Array.concat
+                    [
+                        "credential" |> Encoding.ASCII.GetBytes
+                        masterKey.GetEncoded ()
+                    ]
+
+            digestEngine.BlockUpdate (
+                credentialDigestInput,
+                0,
+                credentialDigestInput.Length
+            )
+
+            digestEngine.DoFinal (credential, 0) |> ignore<int>
+                
+            let! networkStatus = directory.GetNetworkStatus ()
+
+            let blindingFactor =
+                HiddenServicesCipher.CalculateBlindingFactor
+                    (networkStatus.GetTimePeriod ())
+                    (masterKey.GetEncoded ())
+
+            let blindedKey =
+                HiddenServicesCipher.CalculateBlindedPublicKey
+                    blindingFactor
+                    (masterKey.GetEncoded ())
+
+            let subcredentialDigestInput =
+                Array.concat
+                    [
+                        "subcredential" |> Encoding.ASCII.GetBytes
+                        credential
+                        blindedKey
+                    ]
+
+            let subcredential =
+                digestEngine.GetByteLength () |> Array.zeroCreate
+
+            digestEngine.BlockUpdate (
+                subcredentialDigestInput,
+                0,
+                subcredentialDigestInput.Length
+            )
+
+            digestEngine.DoFinal (subcredential, 0) |> ignore<int>
+
+            let intro_secret_hs_input =
+                Array.concat
+                    [
+                        exp
+                        (introductionPointDetails.AuthKey.Public :?> Ed25519PublicKeyParameters).GetEncoded ()
+                        introduce.ClientPublicKey
+                        (introductionPointDetails.EncryptionKey.Public :?> X25519PublicKeyParameters).GetEncoded ()
+                        ProtoId |> Encoding.ASCII.GetBytes
+                    ]
+
+            let infot = Array.concat [ m_hsexpand; subcredential ]
+
+            let finalDigestInput =
+                Array.concat
+                    [
+                        intro_secret_hs_input
+                        t_hsenc
+                        infot
+                    ]
+
+            let hs_keys = digestEngine.GetByteLength () |> Array.zeroCreate
+
+            digestEngine.BlockUpdate (
+                finalDigestInput,
+                0,
+                finalDigestInput.Length
+            )
+
+            digestEngine.DoFinal (hs_keys, 0) |> ignore<int>
+
+            let DEC_KEY = hs_keys |> Array.take 16
+            let MAC_KEY = hs_keys |> Array.skip 16 |> Array.take 16
+
+            let aesCtr = TorStreamCipher(DEC_KEY, None)
+
+            let decryptedData = 
+                introduce.EncryptedData 
+                |> aesCtr.Encrypt
+
+            let expXy = Array.zeroCreate keyAgreement.AgreementSize
+            let expXb = Array.zeroCreate keyAgreement.AgreementSize
+            keyAgreement.CalculateAgreement (randomServerKey.Public :?> X25519PublicKeyParameters, expXy, 0)
+            keyAgreement.CalculateAgreement (introductionPointDetails.EncryptionKey.Public :?> X25519PublicKeyParameters, expXb, 0)
+
+            let rend_secret_hs_input = 
+                Array.concat 
+                    [
+                        expXy
+                        expXb
+                        (introductionPointDetails.AuthKey.Public :?> Ed25519PublicKeyParameters).GetEncoded()
+                        (introductionPointDetails.EncryptionKey.Public :?> X25519PublicKeyParameters).GetEncoded()
+                        introduce.ClientPublicKey
+                        (randomServerKey.Public :?> X25519PublicKeyParameters).GetEncoded()
+                        ProtoId |> Encoding.ASCII.GetBytes
+                    ]
+            let t_hsverify =
+                ProtoId + ":hs_verify" |> Encoding.ASCII.GetBytes
+            let t_hsmac =
+                ProtoId + ":hs_mac" |> Encoding.ASCII.GetBytes
+            let NTOR_KEY_SEED = HiddenServicesCipher.CalculateMacWithSHA3256 rend_secret_hs_input t_hsenc 
+            let verify = HiddenServicesCipher.CalculateMacWithSHA3256 rend_secret_hs_input t_hsverify
+            let auth_input = 
+                Array.concat 
+                    [
+                        verify
+                        (introductionPointDetails.AuthKey.Public :?> Ed25519PublicKeyParameters).GetEncoded()
+                        (introductionPointDetails.EncryptionKey.Public :?> X25519PublicKeyParameters).GetEncoded()
+                        (randomServerKey.Public :?> X25519PublicKeyParameters).GetEncoded()
+                        introduce.ClientPublicKey
+                        ProtoId |> Encoding.ASCII.GetBytes
+                        "Server" |>Encoding.ASCII.GetBytes
+                    ]
+            let _AUTH_INPUT_MAC = HiddenServicesCipher.CalculateMacWithSHA3256 auth_input t_hsmac
+
+            //JOIN RENDEZVOUS
+            ()
+
+        }
+        |> Async.Start
+        
     member self.CreateIntroductionPointAsync
         (introNodeDetail: CircuitNodeDetail)
         =
@@ -83,9 +240,9 @@ type TorServiceHost () =
                             AuthKey = authKeyPair
                             EncryptionKey = encKeyPair
                             NodeDetail = introNodeDetail
+                            Circuit = circuit
+                            Guard = guard
                         }
-
-                    guardNode <- Some guard
 
                     introductionPointKeys <-
                         Map.add
