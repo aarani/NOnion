6f7dfd40a5425facc507033fc770de9be187ef41
Afshin <afshin@arani.dev>
2022-04-22 21:13:53 +0430

    Crypto: replace support for SHA256 with SHA3-256
    This is necessary for HSv3.

--- NOnion/Utility/DigestUtils.fs
+++ NOnion/Utility/DigestUtils.fs
@@ -1,16 +1,17 @@
 ﻿namespace NOnion.Utility
 
+open Org.BouncyCastle.Crypto
 open Org.BouncyCastle.Crypto.Digests
 
 module DigestUtils =
     let CreateDigestInstance
-        (isSha256: bool)
-        (previousDigestOpt: Option<GeneralDigest>)
-        : GeneralDigest =
-        match isSha256, previousDigestOpt with
-        | false, None -> Sha1Digest() :> GeneralDigest
+        (isSha3256: bool)
+        (previousDigestOpt: Option<IDigest>)
+        : IDigest =
+        match isSha3256, previousDigestOpt with
+        | false, None -> Sha1Digest() :> IDigest
         | false, Some previousDigest ->
-            Sha1Digest(previousDigest :?> Sha1Digest) :> GeneralDigest
-        | true, None -> Sha256Digest() :> GeneralDigest
+            Sha1Digest(previousDigest :?> Sha1Digest) :> IDigest
+        | true, None -> Sha3Digest() :> IDigest
         | true, Some previousDigest ->
-            Sha256Digest(previousDigest :?> Sha256Digest) :> GeneralDigest
+            Sha3Digest(previousDigest :?> Sha3Digest) :> IDigest
