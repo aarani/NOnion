660cf798c29cec9ded1ab6f527b95e414cf81d4d
Afshin Arani <afshin@arani.dev>
2022-09-09 15:29:17 +0430

    NOnion, Tests: support for onion-style hs server
    This commit also contains general refactoring of the
    hidden service client code.

--- NOnion/Directory/HiddenServiceDescriptorDocument.fs
+++ NOnion/Directory/HiddenServiceDescriptorDocument.fs
@@ -1,13 +1,17 @@
 ﻿namespace NOnion.Directory
 
 open System
+open System.Text
+
+open NOnion
+open NOnion.Utility
 
 type IntroductionPointEntry =
     {
         OnionKey: Option<array<byte>>
-        AuthKey: Option<array<byte>>
+        AuthKey: Option<Certificate>
         EncKey: Option<array<byte>>
-        EncKeyCert: Option<array<byte>>
+        EncKeyCert: Option<Certificate>
         LinkSpecifiers: Option<array<byte>>
     }
 
@@ -77,6 +81,7 @@ type IntroductionPointEntry =
                             AuthKey =
                                 readBlock String.Empty
                                 |> Convert.FromBase64String
+                                |> Certificate.FromBytes
                                 |> Some
                         }
                 | "enc-key-cert" ->
@@ -90,6 +95,7 @@ type IntroductionPointEntry =
                             EncKeyCert =
                                 readBlock String.Empty
                                 |> Convert.FromBase64String
+                                |> Certificate.FromBytes
                                 |> Some
                         }
 
@@ -97,9 +103,90 @@ type IntroductionPointEntry =
 
         innerParse IntroductionPointEntry.Empty
 
+    override self.ToString() =
+        let strBuilder = StringBuilder()
+
+        let writeByteArray data =
+            let dataInString = data |> Convert.ToBase64String
+
+            let chunkedData =
+                dataInString.ToCharArray()
+                |> Array.chunkBySize Constants.DirectoryBlockLineLength
+                |> Array.map String
+
+            String.Join("\n", chunkedData)
+
+        let appendLine str =
+            strBuilder.Append(sprintf "%s\n" str)
+
+        match self.LinkSpecifiers with
+        | Some linkSpecifier ->
+            appendLine(
+                sprintf
+                    "introduction-point %s"
+                    (Convert.ToBase64String linkSpecifier)
+            )
+            |> ignore<StringBuilder>
+        | None ->
+            failwith
+                "HS document (most inner wrapper) is incomplete, missing linkspecifier"
+
+        match self.OnionKey with
+        | Some onionKey ->
+            appendLine(
+                sprintf "onion-key ntor %s" (Convert.ToBase64String onionKey)
+            )
+            |> ignore<StringBuilder>
+        | None ->
+            failwith
+                "HS document (most inner wrapper) is incomplete, missing OnionKey"
+
+        match self.AuthKey with
+        | Some authKey ->
+            appendLine "auth-key" |> ignore<StringBuilder>
+            appendLine "-----BEGIN ED25519 CERT-----" |> ignore<StringBuilder>
+
+            authKey.ToBytes false
+            |> writeByteArray
+            |> appendLine
+            |> ignore<StringBuilder>
+
+            appendLine "-----END ED25519 CERT-----" |> ignore<StringBuilder>
+        | None ->
+            failwith
+                "HS document (outer wrapper) is incomplete, missing AuthKey"
+
+        match self.EncKey with
+        | Some encKey ->
+            appendLine(
+                sprintf "enc-key ntor %s" (Convert.ToBase64String encKey)
+            )
+            |> ignore<StringBuilder>
+        | None ->
+            failwith
+                "HS document (most inner wrapper) is incomplete, missing EncKey"
+
+        match self.EncKeyCert with
+        | Some encKeyCert ->
+            appendLine "enc-key-cert" |> ignore<StringBuilder>
+            appendLine "-----BEGIN ED25519 CERT-----" |> ignore<StringBuilder>
+
+            encKeyCert.ToBytes false
+            |> writeByteArray
+            |> appendLine
+            |> ignore<StringBuilder>
+
+            appendLine "-----END ED25519 CERT-----" |> ignore<StringBuilder>
+        | None ->
+            failwith
+                "HS document (outer wrapper) is incomplete, missing EncKeyCert"
+
+        strBuilder.ToString()
+
+
 type HiddenServiceDescriptorDocument =
     {
-        Create2Formats: Option<int>
+        Create2Formats: Option<string>
         IsSingleOnionService: bool
 
         IntroductionPoints: List<IntroductionPointEntry>
@@ -111,6 +198,13 @@ type HiddenServiceDescriptorDocument =
             IsSingleOnionService = false
 
             IntroductionPoints = List.empty
+        }
+
+    static member Default =
+        let ntorCreateFormat = "2"
+
+        { HiddenServiceDescriptorDocument.Empty with
+            Create2Formats = Some ntorCreateFormat
         }
 
     static member Parse(stringToParse: string) =
@@ -128,8 +222,8 @@ type HiddenServiceDescriptorDocument =
 
             let words = lines.Peek().Split ' ' |> MutableQueue<string>
 
-            let readInteger() =
-                words.Dequeue() |> int
+            let readRestAsString() =
+                words.ToArray() |> String.concat " "
 
             let newState =
                 match words.Dequeue() with
@@ -138,8 +232,10 @@ type HiddenServiceDescriptorDocument =
 
                     { state with
                         HiddenServiceDescriptorDocument.Create2Formats =
-                            readInteger() |> Some
+                            readRestAsString() |> Some
                     }
+                | "intro-auth-required" ->
+                    failwith "NOnion doesn't support client-authorization"
                 | "single-onion-service" ->
                     lines.Dequeue() |> ignore<string>
 
@@ -163,3 +259,26 @@ type HiddenServiceDescriptorDocument =
                 newState
 
         innerParse HiddenServiceDescriptorDocument.Empty
+
+    override self.ToString() =
+        let strBuilder = StringBuilder()
+
+        let appendLine str =
+            strBuilder.Append(sprintf "%s\n" str)
+
+        match self.Create2Formats with
+        | Some formats ->
+            appendLine(sprintf "create2-formats %s" formats)
+            |> ignore<StringBuilder>
+        | None ->
+            failwith
+                "HS document (most inner wrapper) is incomplete, missing Create2Formats"
+
+        if self.IsSingleOnionService then
+            appendLine "single-onion-service" |> ignore<StringBuilder>
+
+        for introductionPoint in self.IntroductionPoints do
+            strBuilder.Append(introductionPoint.ToString())
+            |> ignore<StringBuilder>
+
+        strBuilder.ToString()
--- NOnion/Directory/HiddenServiceSecondLayerDescriptorDocument.fs
+++ NOnion/Directory/HiddenServiceSecondLayerDescriptorDocument.fs
@@ -1,6 +1,14 @@
 ﻿namespace NOnion.Directory
 
 open System
+open System.Text
+
+open Org.BouncyCastle.Crypto.Parameters
+open Org.BouncyCastle.Crypto.Generators
+open Org.BouncyCastle.Security
+
+open NOnion
+open NOnion.Utility
 
 type HiddenServiceSecondLayerDescriptorDocument =
     {
@@ -52,3 +60,66 @@ type HiddenServiceSecondLayerDescriptorDocument =
                 newState
 
         innerParse HiddenServiceSecondLayerDescriptorDocument.Empty
+
+    override self.ToString() =
+        let writeByteArray data =
+            let dataInString = data |> Convert.ToBase64String
+
+            let chunkedData =
+                dataInString.ToCharArray()
+                |> Array.chunkBySize Constants.DirectoryBlockLineLength
+                |> Array.map String
+
+            String.Join("\n", chunkedData)
+
+        let strBuilder = StringBuilder()
+
+        let appendLine str =
+            strBuilder.Append(sprintf "%s\n" str)
+
+        let ephemeralKey =
+            let kpGen = X25519KeyPairGenerator()
+            let random = SecureRandom()
+            kpGen.Init(X25519KeyGenerationParameters random)
+            let keyPair = kpGen.GenerateKeyPair()
+
+            (keyPair.Public :?> X25519PublicKeyParameters)
+                .GetEncoded()
+            |> Convert.ToBase64String
+
+        appendLine "desc-auth-type x25519" |> ignore<StringBuilder>
+
+        appendLine(sprintf "desc-auth-ephemeral-key %s" ephemeralKey)
+        |> ignore<StringBuilder>
+
+        let authClientLine =
+            let random = SecureRandom()
+
+            let clientId = Array.zeroCreate 8
+            let iv = Array.zeroCreate 16
+            let cookie = Array.zeroCreate 16
+
+            random.NextBytes clientId
+            random.NextBytes iv
+            random.NextBytes cookie
+
+            sprintf
+                "%s %s %s"
+                (Base64Util.EncodeNoPaddding clientId)
+                (Base64Util.EncodeNoPaddding iv)
+                (Base64Util.EncodeNoPaddding cookie)
+
+        appendLine(sprintf "auth-client %s" authClientLine)
+        |> ignore<StringBuilder>
+
+        match self.EncryptedPayload with
+        | Some encrypted ->
+            appendLine "encrypted" |> ignore<StringBuilder>
+            appendLine "-----BEGIN MESSAGE-----" |> ignore<StringBuilder>
+            writeByteArray encrypted |> appendLine |> ignore<StringBuilder>
+            appendLine "-----END MESSAGE-----" |> ignore<StringBuilder>
+        | None ->
+            failwith
+                "HS document (outer wrapper) is incomplete, missing payload"
+
+        strBuilder.ToString()
