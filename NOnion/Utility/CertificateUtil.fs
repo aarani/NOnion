6d2135fa89e07c2a2fc9cdc8802d1ef839016965
Afshin Arani <afshin@arani.dev>
2022-09-09 15:29:17 +0430

    Utility: more complete certifacte util

--- NOnion/Utility/CertificateUtil.fs
+++ NOnion/Utility/CertificateUtil.fs
@@ -3,32 +3,60 @@ namespace NOnion.Utility
 
 open System.IO
 
+type CertificateExtensionType =
+    // https://github.com/torproject/torspec/blob/cb4ae84a20793a00f35a70aad5df47d4e4c7da7c/cert-spec.txt#L175
+    | SignedWithEd25519Key = 4uy
+
 type CertificateExtension =
     {
-        Type: int
+        Type: CertificateExtensionType
         Flags: byte
         Data: array<byte>
     }
 
+    static member Deserialize(reader: BinaryReader) =
+        let dataLength = BinaryIO.ReadBigEndianUInt16 reader |> int
+
+        {
+            CertificateExtension.Type =
+                reader.ReadByte()
+                |> LanguagePrimitives.EnumOfValue<byte, CertificateExtensionType>
+            Flags = reader.ReadByte()
+            Data = reader.ReadBytes dataLength
+        }
+
+    member self.ToBytes() =
+        Array.concat
+            [
+                self.Data.Length
+                |> uint16
+                |> IntegerSerialization.FromUInt16ToBigEndianByteArray
+                Array.singleton(byte self.Type)
+                Array.singleton self.Flags
+                self.Data
+            ]
+
+type CertKeyType =
+    // https://github.com/torproject/torspec/blob/cb4ae84a20793a00f35a70aad5df47d4e4c7da7c/cert-spec.txt#L162
+    | IntroPointAuthKeySignedByDescriptorSigningKey = 9uy
+
 type Certificate =
     {
-        Version: int
-        Type: int
+        Version: byte
+        Type: byte
         ExpirationDate: uint
-        CertKeyType: int
+        CertKeyType: CertKeyType
         CertifiedKey: array<byte>
         Extensions: List<CertificateExtension>
         Signature: array<byte>
     }
 
-    // https://github.com/torproject/torspec/blob/cb4ae84a20793a00f35a70aad5df47d4e4c7da7c/cert-spec.txt#L175
     member self.TryGetSignedWithEd25519Key() =
-        let signedWithExtenstionTypeNum = 4
-
         self.Extensions
-        |> Seq.tryFind(fun ext -> ext.Type = signedWithExtenstionTypeNum)
+        |> Seq.tryFind(fun ext ->
+            ext.Type = CertificateExtensionType.SignedWithEd25519Key
+        )
         |> Option.map(fun ext -> ext.Data)
-
 
     static member Deserialize(reader: BinaryReader) =
         let rec readExtensions
@@ -38,25 +66,35 @@ type Certificate =
             if remainingExtsCount = 0 then
                 state
             else
-                let dataLength = BinaryIO.ReadBigEndianUInt16 reader |> int
-
-                let newExtension =
-                    {
-                        CertificateExtension.Type = reader.ReadByte() |> int
-                        Flags = reader.ReadByte()
-                        Data = reader.ReadBytes dataLength
-                    }
-
                 readExtensions
                     (remainingExtsCount - 1)
-                    (state @ [ newExtension ])
+                    (CertificateExtension.Deserialize reader :: state)
 
         {
-            Version = reader.ReadByte() |> int
-            Type = reader.ReadByte() |> int
+            Version = reader.ReadByte()
+            Type = reader.ReadByte()
             ExpirationDate = BinaryIO.ReadBigEndianUInt32 reader
-            CertKeyType = reader.ReadByte() |> int
+            CertKeyType =
+                reader.ReadByte()
+                |> LanguagePrimitives.EnumOfValue<byte, CertKeyType>
             CertifiedKey = reader.ReadBytes 32
             Extensions = readExtensions (reader.ReadByte() |> int) List.empty
             Signature = reader.ReadBytes 64
         }
+
+    member self.ToBytes() =
+        Array.concat
+            [
+                Array.singleton self.Version
+                Array.singleton self.Type
+                IntegerSerialization.FromUInt32ToBigEndianByteArray
+                    self.ExpirationDate
+                Array.singleton(self.CertKeyType |> byte)
+                self.CertifiedKey
+                Array.singleton(self.Extensions.Length |> byte)
+                self.Extensions
+                |> Seq.map(fun ext -> ext.ToBytes())
+                |> Seq.concat
+                |> Seq.toArray
+                self.Signature
+            ]
