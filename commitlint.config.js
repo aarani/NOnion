3085c51f8586b0f2ca26a3307ce7d0e52aed6e34
Andres G. Aragoneses <knocte@gmail.com>
2022-09-10 15:40:50 +0800

    commitlint: add TODO list for future plugins

--- commitlint.config.js
+++ commitlint.config.js
@@ -13,6 +13,19 @@ module.exports = {
 		'subject-lowercase': [2, 'always'],
 	},
 	plugins: [
+		// TODO (ideas for more rules):
+		// * Don't put space after comma in area/scope (type).
+		// * Don't put space before parentheses or slash in area/scope.
+		// * Better rule than body-max-line-length that ignores line if it starts with `[x] ` where x is a number.
+		// * 'body-full-stop' which finds paragraphs in body without full-stop (which ignores lines in same way as suggested above).
+		// * 'body-paragraph-uppercase' which finds paragraphs in body starting with lowercase.
+		// * Detect if paragraphs in body have been cropped too shortly (less than 64 chars). -> maybe only a warning
+		// * Detect trailing spaces.
+		// * Detect reverts which have not been elaborated.
+		// * Detect WIP commits without a number.
+		// * Reject #XYZ refs in favour for full URLs.
+		// * If full URL for commit found, reject in favour for just the commit hash.
+
 		{
 			rules: {
 				'type-space-after-colon': ({header}) => {
