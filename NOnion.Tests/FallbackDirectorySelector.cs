24bec13bbe34aa5217f2a49aecae0389cd5caeb8
Afshin Arani <afshin@arani.dev>
2022-09-11 19:10:46 +0430

    Tests: always download latest fallback dir list

---
+++ NOnion.Tests/FallbackDirectorySelector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using FSharpx.Collections;
+using System.Runtime.Intrinsics.X86;
+using System.Text.RegularExpressions;
+
+namespace NOnion.Tests
+{
+    public static class FallbackDirectorySelector
+    {
+        static List<string> fallbackDirectories;
+
+        static internal IPEndPoint GetRandomFallbackDirectory()
+        {
+            if (fallbackDirectories == null)
+            {
+                var urlToTorServerList = "https://raw.githubusercontent.com/torproject/tor/main/src/app/config/fallback_dirs.inc";
+                using var webClient = new WebClient();
+                var fetchedInfo = webClient.DownloadString(urlToTorServerList);
+
+                var ipv4Pattern = "\"([0-9\\.]+)\\sorport=(\\S*)\\sid=(\\S*)\"";
+                var matches = Regex.Matches(fetchedInfo, ipv4Pattern);
+
+                fallbackDirectories = matches.Select(regMatch => $"{regMatch.Groups[1].Value}:{regMatch.Groups[2].Value}").ToList();
+            }
+
+            return
+                IPEndPoint.Parse (
+                    fallbackDirectories
+                        .OrderBy(x => Guid.NewGuid())
+                        .First()
+                );
+        }
+    }
+}
