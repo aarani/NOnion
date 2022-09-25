7a9732ecc7e6d506650327502c558a8fe01ddbd3
Andres G. Aragoneses <knocte@gmail.com>
2022-09-26 00:34:02 +0800

    commitlint: tests for type-space-after-colon

--- commitlintplugins.test.ts
+++ commitlintplugins.test.ts
@@ -10,3 +10,24 @@ test('spawnSync2', () => {
     expect(unexistent.error).not.toBe(undefined);
 });
 
+let commitMsgWithNoSpace = 'foo:bar'
+test('type-space-after-colon1', () => {
+    let typeSpaceAfterColon1 = spawnSync('npx', ['commitlint', '--verbose'], { input: commitMsgWithNoSpace });
+
+    expect(typeSpaceAfterColon1.status).not.toBe(0);
+});
+
+let commitMsgWithSpace = 'foo: bar'
+test('type-space-after-colon2', () => {
+    let typeSpaceAfterColon2 = spawnSync('npx', ['commitlint', '--verbose'], { input: commitMsgWithSpace });
+
+    expect(typeSpaceAfterColon2.status).toBe(0);
+});
+
+let commitMsgWithNoSpaceBeforeColonButAtTheEnd = 'foo: a tale of bar:baz'
+test('type-space-after-colon3', () => {
+    let typeSpaceAfterColon3 = spawnSync('npx', ['commitlint', '--verbose'], { input: commitMsgWithNoSpaceBeforeColonButAtTheEnd });
+
+    //console.log("=============>" + typeSpaceAfterColon3.stdout);
+    expect(typeSpaceAfterColon3.status).toBe(0);
+});
