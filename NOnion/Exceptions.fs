22841d0a0a90eeada5e015836f621d71ad8d5053
webwarrior <reg@webwarrior.ws>
2022-10-04 14:51:29 +0200

    Make constructors of exceptions internal

--- NOnion/Exceptions.fs
+++ NOnion/Exceptions.fs
@@ -5,40 +5,44 @@ open System
 type NOnionException =
     inherit Exception
 
-    new(msg: string) = { inherit Exception(msg) }
+    internal new(msg: string) = { inherit Exception(msg) }
 
-    new(msg: string, innerException: Exception) =
+    internal new(msg: string, innerException: Exception) =
         { inherit Exception(msg, innerException) }
 
 type GuardConnectionFailedException =
     inherit NOnionException
 
-    new(innerException: Exception) =
+    internal new(innerException: Exception) =
         { inherit NOnionException("Connecting to guard node failed",
                                   innerException) }
 
-    new(message: string) =
+    internal new(message: string) =
         { inherit NOnionException("Connecting to guard node failed: " + message) }
 
 
-type CircuitTruncatedException(reason: DestroyReason) =
+type CircuitTruncatedException internal (reason: DestroyReason) =
     inherit NOnionException(sprintf "Circuit got truncated, reason %A" reason)
 
-type CircuitDestroyedException(reason: DestroyReason) =
+type CircuitDestroyedException internal (reason: DestroyReason) =
     inherit NOnionException(sprintf "Circuit got destroyed, reason %A" reason)
 
-type TimeoutErrorException() =
+type TimeoutErrorException internal () =
     inherit NOnionException("Time limit exceeded for operation")
 
-type UnsuccessfulHttpRequestException(statusCode: string) =
+type UnsuccessfulHttpRequestException internal (statusCode: string) =
     inherit NOnionException(sprintf
                                 "Non-200 status code received, code: %s"
                                 statusCode)
 
-type UnsuccessfulIntroductionException(status: RelayIntroduceStatus) =
+type UnsuccessfulIntroductionException internal (status: RelayIntroduceStatus) =
     inherit NOnionException(sprintf "Unsuccessful introduction: %A" status)
 
-type NOnionSocketException(innerException: Net.Sockets.SocketException) =
+type NOnionSocketException
+    internal
+    (
+        innerException: Net.Sockets.SocketException
+    ) =
     inherit NOnionException
         (
             "Got socket exception during data transfer",
