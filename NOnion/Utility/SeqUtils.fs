f28e2ff7bf52dfbc61d1d7d5e3f345ac22751e59
Afshin Arani <afshin@arani.dev>
2023-04-08 15:11:31 +0330

    Network: use F#'s ChunkBySize method
    This commit removes the chunk helper function
    in favor the already existing F# method named
    ChunkBySize, not sure why I didn't find it before.

--- NOnion/Utility/SeqUtils.fs
+++ NOnion/Utility/SeqUtils.fs
@@ -1,12 +1,5 @@
 ﻿namespace NOnion.Utility
 
 module SeqUtils =
-    // Helper function copied from https://stackoverflow.com/a/21615676
-    let Chunk chunkSize sequence =
-        sequence
-        |> Seq.mapi(fun i x -> i / chunkSize, x)
-        |> Seq.groupBy fst
-        |> Seq.map(fun (_, g) -> Seq.map snd g)
-
     let TakeRandom count sequence =
         sequence |> Seq.sortBy(fun _ -> System.Guid.NewGuid()) |> Seq.take count
