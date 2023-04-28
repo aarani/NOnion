dc219ae8dbd4faf908652eb801673e352ee35dd8
Afshin Arani <afshin@arani.dev>
2023-04-28 20:23:30 +0330

    Directory,Utility: use binary search
    This commit changes the search algorithm
    for finding hs directories from linear to
    binary search.

--- NOnion/Directory/TorDirectory.fs
+++ NOnion/Directory/TorDirectory.fs
@@ -12,6 +12,7 @@ open NOnion.Crypto
 open NOnion.Network
 open NOnion.Http
 open NOnion.Utility
+open NOnion.Utility.ByteArrayUtil
 
 type RouterType =
     | Normal
@@ -456,33 +457,6 @@ type TorDirectory =
 
             let! networkStatus = self.GetLiveNetworkStatus()
 
-            let ByteArrayCompare (x: array<byte>) (y: array<byte>) =
-                let xlen = x.Length
-                let ylen = y.Length
-
-                let len =
-                    if xlen < ylen then
-                        xlen
-                    else
-                        ylen
-
-                let mutable index = 0
-                let mutable result = 0
-
-                while index < len do
-                    let diff = (int(x.[index])) - int(y.[index])
-
-                    if diff <> 0 then
-                        index <- len + 1 // breaks out of the loop, and signals that result is valid
-                        result <- diff
-                    else
-                        index <- index + 1
-
-                if index > len then
-                    result
-                else
-                    (xlen - ylen)
-
             let directories =
                 networkStatus.GetHiddenServiceDirectories()
                 |> List.choose(fun node ->
@@ -528,13 +502,31 @@ type TorDirectory =
                             ]
                         |> HiddenServicesCipher.SHA3256
 
-                    //FIXME: binary serach idx here
                     let start =
-                        directories
-                        |> Seq.tryFindIndex(fun (_, index) ->
-                            ByteArrayCompare index hsIndex >= 0
-                        )
-                        |> Option.defaultValue 0
+                        let maybeStart =
+                            let hsIndexes =
+                                directories
+                                |> Array.ofList
+                                |> Array.map(fun (_, hsIndex) -> hsIndex)
+
+                            Array.BinarySearch(
+                                hsIndexes,
+                                hsIndex,
+                                ByteArrayComparer()
+                            )
+
+                        if maybeStart < 0 then
+                            let complement = ~~~maybeStart
+
+                            // Getting the length of the list if no member is greater than the key we
+                            // are looking for so start at the first element.
+                            if complement >= directories.Length then
+                                let arrayStartIndex = 0
+                                arrayStartIndex
+                            else
+                                complement
+                        else
+                            maybeStart
 
                     let rec pickNodes startIndex nToAdd state =
                         if nToAdd = 0 then
---
+++ NOnion/Utility/ByteArrayUtil.fs
@@ -0,0 +1,36 @@
+﻿namespace NOnion.Utility
+
+open System.Collections.Generic
+
+module ByteArrayUtil =
+    let ByteArrayCompare (x: array<byte>) (y: array<byte>) =
+        let xlen = x.Length
+        let ylen = y.Length
+
+        let len =
+            if xlen < ylen then
+                xlen
+            else
+                ylen
+
+        let mutable index = 0
+        let mutable result = 0
+
+        while index < len do
+            let diff = (int(x.[index])) - int(y.[index])
+
+            if diff <> 0 then
+                index <- len + 1 // breaks out of the loop, and signals that result is valid
+                result <- diff
+            else
+                index <- index + 1
+
+        if index > len then
+            result
+        else
+            (xlen - ylen)
+
+    type ByteArrayComparer() =
+        interface IComparer<array<byte>> with
+            member __.Compare(first, second) =
+                ByteArrayCompare first second
